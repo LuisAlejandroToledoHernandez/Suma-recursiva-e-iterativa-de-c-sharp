30d4efaf1fc9b5c5d2364387f6ec969d42714d4f
Alex5 <Alex5@TOLEDO521>
2023-10-28 14:20:20 -0600

    Agregar archivos de proyecto.

---
+++ Suma recursiva e iterativa de c sharp/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+class Program
+{
+    static int SumaIterativa(int n)
+    {
+        int suma = 0;
+        while (n > 9)
+        {
+            suma += n % 10;
+            n /= 10;
+        }
+        return suma + n;
+    }
+
+    static int SumaRecursiva(int n)
+    {
+        if (n <= 9)
+            return n;
+        else
+            return SumaRecursiva(n / 10) + n % 10;
+    }
+
+    static void Main()
+    {
+        int numero;
+
+        Console.Write("Ingresa un número: ");
+        numero = Convert.ToInt32(Console.ReadLine());
+
+        int sumaIterativaResultado = SumaIterativa(numero);
+        int sumaRecursivaResultado = SumaRecursiva(numero);
+
+        Console.WriteLine("Suma iterativa: " + sumaIterativaResultado);
+        Console.WriteLine("Suma recursiva: " + sumaRecursivaResultado);
+
+        Console.ReadLine();
+    }
+}
